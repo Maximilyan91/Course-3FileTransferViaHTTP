a0b8f2893b7d325bc88371994aee061d0c479588
Максим <gmax7911@gmail.com>
2023-02-21 15:45:02 +0300

    в процессе

---
+++ src/main/java/com/cooking/course3_recipeApp/service/impl/RecipeServiceImpl.java
@@ -0,0 +1,26 @@
+package com.cooking.course3_recipeApp.service.impl;
+
+import com.cooking.course3_recipeApp.model.Recipe;
+import com.cooking.course3_recipeApp.service.RecipeService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RecipeServiceImpl implements RecipeService {
+
+
+    private Map<Integer, Recipe> recipes = new HashMap<>();
+
+
+
+
+    @Override
+    public void addRecipe(Recipe recipe) {
+
+    }
+
+    @Override
+    public void getRecipe(int id) {
+
+    }
+}
